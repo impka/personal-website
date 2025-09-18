751af4b97f56d125c563b4c552a72ce698ff23d1
impka <imperialistkarl@gmail.com>
2025-09-17 21:28:29 -0700

    blogs page init

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,13 @@
+import styles from './page.module.css'
+
+function Blogs(){
+    return (
+        <>
+            <div className='grid min-h-screen items-center justify-items-center'>
+                <h1>Hi</h1>
+            </div>
+        </>
+    )
+}
+
+export default Blogs
