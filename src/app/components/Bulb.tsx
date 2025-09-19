f88e7c69bcc56b07b4874ed9c02ad75f2e64325e
impka <imperialistkarl@gmail.com>
2025-09-19 12:37:37 -0700

    added draggable light bulb

--- src/app/components/Bulb.tsx
+++ src/app/components/Bulb.tsx
@@ -1,10 +1,49 @@
 'use client'
 
-import { useGLTF } from "@react-three/drei"
+import { useEffect } from "react"
+import { useGLTF, Center } from "@react-three/drei"
+import { useDrag } from "@use-gesture/react"
+import { a, useSpring } from "@react-spring/three"
+import * as THREE from 'three'
 
 export default function Bulb() {
-  const { scene } = useGLTF("/incandescent_light_bulb.glb")
-  return (
-    <primitive object={scene} scale={75} position={[-7,-5,0]}/>
-  );
+
+    const { scene } = useGLTF("/incandescent_light_bulb.glb")
+
+    useEffect(() => {
+        scene.traverse((child) => {
+          if (child instanceof THREE.Mesh) {
+            const mat = child.material as THREE.MeshStandardMaterial
+            mat.emissive = new THREE.Color("#ffcc66")
+            mat.emissiveIntensity = 0.05
+          }
+        })
+    }, [scene]);
+
+    const [spring, api] = useSpring(() => ({
+        rotX: 0,
+        rotY: 0,
+        rotZ: 0,
+        config: { mass: 1, tension: 170, friction: 26 },
+    }))
+
+    const bind = useDrag(({ movement: [x, y] }) => {
+        api.start({ rotX: 0, rotY: x / 100, rotZ: 0 })
+    })
+
+    return (
+        <a.group {...bind()} rotation-x={spring.rotX} rotation-y={spring.rotY} rotation-z={spring.rotZ} position={[-20, 0, 0]}>
+            <Center>
+                <primitive 
+                    object={scene} 
+                    scale={150}
+                />
+                <pointLight 
+                    castShadow 
+                    intensity={10} 
+                    distance={1000} 
+                    color="white" />
+            </Center>
+        </a.group>
+    )
 }
--- src/app/components/Scene.tsx
+++ src/app/components/Scene.tsx
@@ -1,14 +1,21 @@
 'use client'
 
-import { Environment } from "@react-three/drei"
+import { Environment, Plane } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
 import Bulb from './Bulb'
 
 export default function Scene() {
   return (
-    <Canvas camera={{position: [0, 0, 20], fov:50}}>
-        <ambientLight intensity={1}/>
+    <Canvas shadows camera={{position: [0, 0, 50], fov:50}}>
         <Bulb/>
+        <Plane
+            castShadow
+            args={[1000,1000]}
+            rotation={[-Math.PI / 2, 0, 0]}
+            position={[0, -25, 0]}
+        >
+            <meshStandardMaterial color="lightgray"/>
+        </Plane>
         <Environment preset="city" background={false} />
     </Canvas>
   );
