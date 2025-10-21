93ba96543f68133e894526b74b0bf5c6a2746c75
impka <imperialistkarl@gmail.com>
2025-10-20 21:24:15 -0700

    changed blog list items display

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -8,16 +8,16 @@ function Blogs(){
         <>
             <div className='min-h-screen p-1'>
                 <div className="m-2 p-1 border-b">
-                    <Link className="underline-hover" href="/"> Back Home</Link>
+                    <Link className="underline-hover text-xl" href="/"> Back Home</Link>
                 </div>
                 <div>
-                    <ul>
+                    <ul className='flex flex-col gap-3 px-3 text-xl'>
                         {blogs.map((blog) => (
-                            <li key={blog.id}>
-                                <Link href={`/blogs/${blog.id}`}>
-                                    {blog.title}
-                                </Link>
-                            </li>
+                            <Link key={blog.id} href={`/blogs/${blog.id}`}>
+                                <li className='bg-white p-2 rounded-lg shadow-md transition duration-250 ease-in-out hover:bg-[#F0F0F0] hover:scale-101'>
+                                        {blog.title}
+                                </li>
+                            </Link>
                         ))}
                     </ul>
                 </div>
